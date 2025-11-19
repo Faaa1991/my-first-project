6f045ef34306286d5d59d5f3ce36223e8fd2fe8a
Fahime <136555669+Faaa1991@users.noreply.github.com>
2025-11-19 20:49:04 +0330

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,1 +1,14 @@
 print("Simple TODO App")
+todos = []
+
+def show_menu():
+    print("1. Add TODO")
+    print("2. List TODOs")
+    print("3. Exit")
+
+def add_todo(item):
+    todos.append(item)
+
+def list_todos():
+    for i, t in enumerate(todos):
+        print(f"{i+1}. {t}")
